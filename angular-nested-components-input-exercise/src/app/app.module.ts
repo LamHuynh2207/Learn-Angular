9f5c00d9e17b1f6d7582705c22dafe00273ec1be
LamHuynh <lamhuynh2207@gmail.com>
2023-05-26 17:52:28 +0700

    Exercise 2

---
+++ angular-nested-components-input-exercise/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { PostsComponent } from './posts/posts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyProfileComponent,
+    PostsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular-nested-components-input-exercise/src/app/my-profile/my-profile.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-my-profile',
+  templateUrl: './my-profile.component.html',
+  styleUrls: ['./my-profile.component.css']
+})
+export class MyProfileComponent {
+  @Input() postsData: string[] | undefined;
+}
